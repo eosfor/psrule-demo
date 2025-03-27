6ed02f3716431b3e382c5df4e326eee495d66ffd
eosfor <9363027+eosfor@users.noreply.github.com>
2025-03-27 05:54:55 +0000

    psrule stuff

--- .ps-rule/local.ArchitectureVerification.Rule.ps1
+++ .ps-rule/local.ArchitectureVerification.Rule.ps1
@@ -1,3 +1,4 @@
 Rule 'local.Architecture.Verification' -Type 'Microsoft.Network/virtualNetworks' {
+    # this aims to check if network exisists as a resource
     $Assert.HasFieldValue($TargetObject, 'name', 'vnetDeployment')
 }
