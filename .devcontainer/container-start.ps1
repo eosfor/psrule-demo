b30c448a370f4f1bdfd9109ac61db9fd8aa8d98d
eosfor <9363027+eosfor@users.noreply.github.com>
2025-06-22 01:49:13 +0000

    making it work on a mac

--- .devcontainer/container-start.ps1
+++ .devcontainer/container-start.ps1
@@ -1,11 +1,31 @@
 # Update modules
-Update-Module PSRule.Rules.Azure -Scope CurrentUser -Force;
+# Update-Module PSRule.Rules.Azure -Scope CurrentUser -Force;
 
-# Fetch the latest Bicep CLI binary
-curl -Lo bicep https://github.com/Azure/bicep/releases/latest/download/bicep-linux-x64
-# Mark it as executable
-chmod +x ./bicep
-# Add bicep to the PATH (requires admin)
-sudo mv ./bicep /usr/local/bin/bicep
-# Verify you can now access the 'bicep' command
+# Определить ОС
+if ([System.Runtime.InteropServices.RuntimeInformation]::IsOSPlatform([System.Runtime.InteropServices.OSPlatform]::OSX)) {
+    $os = "osx"
+} elseif ([System.Runtime.InteropServices.RuntimeInformation]::IsOSPlatform([System.Runtime.InteropServices.OSPlatform]::Linux)) {
+    $os = "linux"
+} else {
+    throw "Unsupported OS"
+}
+
+# Определить архитектуру
+$arch = [System.Runtime.InteropServices.RuntimeInformation]::OSArchitecture
+
+switch ($arch) {
+    "X64"   { $platform = "$os-x64" }
+    "Arm64" { $platform = "$os-arm64" }
+    default { throw "Unsupported architecture: $arch" }
+}
+
+# Скачать нужный бинарник
+$version = "latest"
+$bicepUrl = "https://github.com/Azure/bicep/releases/$version/download/bicep-$platform"
+$destination = "$PWD/bicep"
+
+Invoke-WebRequest -Uri $bicepUrl -OutFile $destination
+chmod +x $destination
+sudo mv $destination /usr/local/bin/bicep
+
 bicep --version
